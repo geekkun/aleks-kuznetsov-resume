7a5b758ea5361c2d31387c6e1796a3ff06ea787c
a.kuznetsov <a.kuznetsov@robo.finance>
2021-02-16 17:13:28 +0300

    Update

--- pages/index.js
+++ pages/index.js
@@ -14,66 +14,120 @@ export default function Home() {
                     Aleks Kuznetsov
                 </h1>
 
-                    <p className={styles.description}>
-                        {/*Get started by editing{' '}*/}
-                        {/*<code className={styles.code}>pages/index.js</code>*/}
-                        Contact me via <a target="_blank"
-                    rel="noopener noreferrer" href="mailto:me@alekskuznetsov.com">email</a> <a target="_blank"
+                <p className={styles.description}>
+                    {/*Get started by editing{' '}*/}
+                    {/*<code className={styles.code}>pages/index.js</code>*/}
+                    Contact me via <a target="_blank"
+                                      rel="noopener noreferrer" href="mailto:me@alekskuznetsov.com">email</a> <a
+                    target="_blank"
                     rel="noopener noreferrer" href="https://t.me/gk_kn">telegram</a> <a target="_blank"
-                    rel="noopener noreferrer" href="https://twitter.com/the_kuznetsov">twitter</a>
+                                                                                        rel="noopener noreferrer"
+                                                                                        href="https://twitter.com/the_kuznetsov">twitter</a>
 
-                    </p>
+                </p>
+                <h3 className={styles.subtitle}> Personal Projects</h3>
+                <div className={styles.grid}>
+                    <a href="https://visas.digital" className={styles.card}>
+                        <h3>
+                            <img src="/visas.png" alt="Visas Digital Logo" className={styles.logo}/>
+                            <span> &nbsp;Visas Digital &rarr;</span>
+                        </h3>
+                        <p>Manage your travel documents and get new visas at ease</p>
+                    </a>
 
-                {/*<div className={styles.grid}>*/}
-                {/*    <a href="https://nextjs.org/docs" className={styles.card}>*/}
-                {/*        <h3>Documentation &rarr;</h3>*/}
-                {/*        <p>Find in-depth information about Next.js features and API.</p>*/}
-                {/*    </a>*/}
+                    <a href="https://wish.surf" className={styles.card}>
+                        <h3><img src="/surf.png" alt="Wish Surf Logo" className={styles.logo}/> &nbsp; Wish Surf &rarr;
+                        </h3>
+                        <p>Don't limit your wishes to purchases, dream wider!</p>
+                    </a>
 
-                {/*    <a href="https://nextjs.org/learn" className={styles.card}>*/}
-                {/*        <h3>Learn &rarr;</h3>*/}
-                {/*        <p>Learn about Next.js in an interactive course with quizzes!</p>*/}
-                {/*    </a>*/}
+                    {/*<a*/}
+                    {/*    href="https://github.com/vercel/next.js/tree/master/examples"*/}
+                    {/*    className={styles.card}*/}
+                    {/*>*/}
+                    {/*    <h3>Examples &rarr;</h3>*/}
+                    {/*    <p>Discover and deploy boilerplate example Next.js projects.</p>*/}
+                    {/*</a>*/}
 
-                {/*    <a*/}
-                {/*        href="https://github.com/vercel/next.js/tree/master/examples"*/}
-                {/*        className={styles.card}*/}
-                {/*    >*/}
-                {/*        <h3>Examples &rarr;</h3>*/}
-                {/*        <p>Discover and deploy boilerplate example Next.js projects.</p>*/}
-                {/*    </a>*/}
-
-                {/*    <a*/}
-                {/*        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
-                {/*        className={styles.card}*/}
-                {/*    >*/}
-                {/*        <h3>Deploy &rarr;</h3>*/}
-                {/*        <p>*/}
-                {/*            Instantly deploy your Next.js site to a public URL with Vercel.*/}
-                {/*        </p>*/}
-                {/*    </a>*/}
-                {/*</div>*/}
+                    {/*<a*/}
+                    {/*    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
+                    {/*    className={styles.card}*/}
+                    {/*>*/}
+                    {/*    <h3>Deploy &rarr;</h3>*/}
+                    {/*    <p>*/}
+                    {/*        Instantly deploy your Next.js site to a public URL with Vercel.*/}
+                    {/*    </p>*/}
+                    {/*</a>*/}
+                </div>
+                <h3 className={styles.subtitle}> Skills</h3>
+                <div className={styles.grid}>
+                    <a href="https://visas.digital" className={styles.bubble}>
+                        <h3>
+                            Python
+                        </h3>
+                    </a>
+                    <a href="https://wish.surf" className={styles.bubble}>
+                        <h3>Vue.JS</h3>
+                    </a>
+                    <a href="https://visas.digital" className={styles.bubble}>
+                        <h3>
+                            Python
+                        </h3>
+                    </a>
+                    <a href="https://wish.surf" className={styles.bubble}>
+                        <h3>Vue.JS</h3>
+                    </a>
+                    <a href="https://visas.digital" className={styles.bubble}>
+                        <h3>
+                            Python
+                        </h3>
+                    </a>
+                    <a href="https://wish.surf" className={styles.bubble}>
+                        <h3>Vue.JS</h3>
+                    </a>
+                    <a href="https://visas.digital" className={styles.bubble}>
+                        <h3>
+                            Python
+                        </h3>
+                    </a>
+                    <a href="https://wish.surf" className={styles.bubble}>
+                        <h3>Vue.JS</h3>
+                    </a>
+                    <a href="https://visas.digital" className={styles.bubble}>
+                        <h3>
+                            Python
+                        </h3>
+                    </a>
+                    <a href="https://wish.surf" className={styles.bubble}>
+                        <h3>Vue.JS</h3>
+                    </a>
+                    <a href="https://wish.surf" className={styles.bubble}>
+                        <h3>Vue.JS</h3>
+                    </a>
+                </div>
+                <h3 className={styles.subtitle}> Work Experience</h3>
+                <h3 className={styles.subtitle}> Education</h3>
             </main>
 
-            <footer className={styles.footer}>
-                <a
-                    href="https://visas.digital"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Visas Digital {' '}
-                    {/*<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />*/}
-                </a>
-                <span>|</span>
-                <a
-                    href="https://wish.surf"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Wish Surf
-                    {/*<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />*/}
-                </a>
-            </footer>
+            {/*<footer className={styles.footer}>*/}
+                {/*<a*/}
+                {/*    href="https://visas.digital"*/}
+                {/*    target="_blank"*/}
+                {/*    rel="noopener noreferrer"*/}
+                {/*>*/}
+                {/*    Visas Digital {' '}*/}
+                {/*    /!*<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />*!/*/}
+                {/*</a>*/}
+                {/*<span>|</span>*/}
+                {/*<a*/}
+                {/*    href="https://wish.surf"*/}
+                {/*    target="_blank"*/}
+                {/*    rel="noopener noreferrer"*/}
+                {/*>*/}
+                {/*    Wish Surf*/}
+                {/*    /!*<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />*!/*/}
+                {/*</a>*/}
+            {/*</footer>*/}
         </div>
     )
 }
