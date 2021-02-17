691c09f00cfe70eda2b0edc96911d334e4e252a8
a.kuznetsov <a.kuznetsov@robo.finance>
2021-02-17 16:58:12 +0300

    Update education

--- pages/index.js
+++ pages/index.js
@@ -30,13 +30,13 @@ export default function Home() {
                     <a href="https://visas.digital" className={styles.card}>
                         <h3>
                             <img src="/visas.png" alt="Visas Digital Logo" className={styles.logo}/>
-                            <span> &nbsp;Visas Digital &rarr;</span>
+                            <span> &nbsp;Visas Digital </span>
                         </h3>
                         <p>Manage your travel documents and get new visas at ease</p>
                     </a>
 
                     <a href="https://wish.surf" className={styles.card}>
-                        <h3><img src="/surf.png" alt="Wish Surf Logo" className={styles.logo}/> &nbsp; Wish Surf &rarr;
+                        <h3><img src="/surf.png" alt="Wish Surf Logo" className={styles.logo}/> &nbsp; Wish Surf
                         </h3>
                         <p>Don't limit your wishes to purchases, dream wider!</p>
                     </a>
@@ -66,6 +66,7 @@ export default function Home() {
                             Python
                         </h3>
                     </a>
+                    {/*<span className={styles.divider} />*/}
                     <a href="https://wish.surf" className={styles.bubble}>
                         <h3>Vue.JS</h3>
                     </a>
@@ -106,27 +107,74 @@ export default function Home() {
                     </a>
                 </div>
                 <h3 className={styles.subtitle}> Work Experience</h3>
+                <div className={styles.grid}>
+                    <a href="https://visas.digital" className={styles.card}>
+                        <h3>
+                            <img src="/visas.png" alt="Visas Digital Logo" className={styles.logo}/>
+                            <span> &nbsp;Visas Digital &rarr;</span>
+                        </h3>
+                        <p>Manage your travel documents and get new visas at ease</p>
+                    </a>
+
+                    <a href="https://wish.surf" className={styles.card}>
+                        <h3><img src="/surf.png" alt="Wish Surf Logo" className={styles.logo}/> &nbsp; Wish Surf
+                        </h3>
+                        <p>Don't limit your wishes to purchases, dream wider!</p>
+                    </a>
+                </div>
                 <h3 className={styles.subtitle}> Education</h3>
+                <div className={styles.grid}>
+                    <a href="https://www.qmul.ac.uk/" className={styles.card}>
+                        <h3>
+                            <img src="/qmul.png" alt="Queen Mary University of London Logo" className={styles.logo}/>
+                            <span> &nbsp;Queen Mary University of London</span>
+                        </h3>
+                        {/*<p>Manage your travel documents and get new visas at ease</p>*/}
+                    </a>
+
+                    <a href="https://www.mpw.ac.uk/" className={styles.card}>
+                        <h3><img src="/mpw.jpg" alt="Mander Portman Woodward Logo" className={styles.logo}/>
+                        <span> &nbsp; Mander Portman Woodward</span>
+                        </h3>
+                        {/*<p>Don't limit your wishes to purchases, dream wider!</p>*/}
+                    </a>
+                    <a href="https://www.windermereschool.co.uk/" className={styles.card}>
+                        <h3>
+                            <img src="/windermere.png" alt="Windermere School Logo" className={styles.logo}/>
+                            <span> &nbsp;Windermere School</span>
+                            <br />
+                            <span>&nbsp;</span>
+                        </h3>
+                        {/*<p>Manage your travel documents and get new visas at ease</p>*/}
+                    </a>
+
+                    <a href="http://www.chastnayashkola.ru/" className={styles.card}>
+                        <h3><img src="/lomonosov.jpg" alt="Mander Portman Woodward Logo" className={styles.logo}/>
+                        <span> &nbsp; Private Lomonosov School</span>
+                        </h3>
+                        {/*<p>Don't limit your wishes to purchases, dream wider!</p>*/}
+                    </a>
+                </div>
             </main>
 
             {/*<footer className={styles.footer}>*/}
-                {/*<a*/}
-                {/*    href="https://visas.digital"*/}
-                {/*    target="_blank"*/}
-                {/*    rel="noopener noreferrer"*/}
-                {/*>*/}
-                {/*    Visas Digital {' '}*/}
-                {/*    /!*<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />*!/*/}
-                {/*</a>*/}
-                {/*<span>|</span>*/}
-                {/*<a*/}
-                {/*    href="https://wish.surf"*/}
-                {/*    target="_blank"*/}
-                {/*    rel="noopener noreferrer"*/}
-                {/*>*/}
-                {/*    Wish Surf*/}
-                {/*    /!*<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />*!/*/}
-                {/*</a>*/}
+            {/*<a*/}
+            {/*    href="https://visas.digital"*/}
+            {/*    target="_blank"*/}
+            {/*    rel="noopener noreferrer"*/}
+            {/*>*/}
+            {/*    Visas Digital {' '}*/}
+            {/*    /!*<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />*!/*/}
+            {/*</a>*/}
+            {/*<span>|</span>*/}
+            {/*<a*/}
+            {/*    href="https://wish.surf"*/}
+            {/*    target="_blank"*/}
+            {/*    rel="noopener noreferrer"*/}
+            {/*>*/}
+            {/*    Wish Surf*/}
+            {/*    /!*<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />*!/*/}
+            {/*</a>*/}
             {/*</footer>*/}
         </div>
     )
